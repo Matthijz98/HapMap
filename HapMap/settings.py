31a3a35e5761e4d9f103709d91da0bee56d02131
Matthijs <mzondervan@hotmail.nl>
2019-11-18 23:52:00 +0100

    added Debug false to production settings

--- HapMap/settings.py
+++ HapMap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+dev = (os.environ['DEV'], False)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,9 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pu!ghk(+g4oc_@pf@+66di797*_#+i^_-3!mo@j20-^lu756m5'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -103,10 +101,6 @@ WSGI_APPLICATION = 'HapMap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-print(os.environ)
-dev = (os.environ['DEV'])
-
-
 if dev == False:
     DATABASES = {
         'default': {
@@ -118,6 +112,8 @@ if dev == False:
             'PORT': 5432,
         }
     }
+
+    DEBUG = False
 else:
     DATABASES = {
         'default': {
@@ -125,7 +121,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+    DEBUG = True
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
