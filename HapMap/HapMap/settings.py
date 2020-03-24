701af3ede3ff686305afa6687a7169f83c10ec41
Matthijs Zondervan <mzondervan@hotmail.nl>
2020-03-24 12:18:57 +0100

    add created_at, updated_at, created_by fields

--- HapMap/HapMap/settings.py
+++ HapMap/HapMap/settings.py
@@ -166,12 +166,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nl'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
+# DATE_FORMAT = "Y-m-d"
+# DATETIME_FORMAT = 'd-m-Y H:M:S'
+
 
 USE_TZ = True
