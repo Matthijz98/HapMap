12587866096aa6f1ef604d67fa2ed116156fca58
Matthijs Zondervan <mzondervan@hotmail.nl>
2024-05-07 17:41:06 +0200

    start on store

--- src/components/eaters/Eaters.tsx
+++ src/components/eaters/Eaters.tsx
@@ -1,7 +1,20 @@
+import { useStore } from '@nanostores/react';
+import { persons } from '../stores/eatersStore';
+
 export default function Eaters(){
-    return <div className={'bg-slate-200 rounded p-2 mb-2'}>
-        <div className={'flex gap-4'}>
-            <label>Eters</label><input type={'number'} className={'w-full'}/>
+    const allergies = useStore(persons);
+
+    return (
+        <div className={'bg-slate-200 rounded p-2 mb-2'}>
+            <div className={'flex gap-4'}>
+                <div>Eaters</div>
+                {Object.keys(allergies).map(allergy => (
+                    <div key={allergy}>
+                        <label>{allergy}</label>
+                        <input type={'number'} className={'w-full'} />
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
+    );
 }
--- src/components/stores/eatersStore.ts
+++ src/components/stores/eatersStore.ts
@@ -1,3 +1,28 @@
-import { atom } from 'nanostores';
+import { persistentAtom } from '@nanostores/persistent'
+import {getCollection} from "astro:content";
 
-export const persons = atom({});
+
+// Define a function to get all allergies from ingredients
+async function getAllergies() {
+    const ingredients = await getCollection('allergies');
+    let allergies = ingredients.map((ingredient) => ingredient.data.name);
+
+    // Remove duplicates
+    allergies = [...new Set(allergies)];
+
+    return allergies;
+}
+
+const allergies = await getAllergies();
+
+// Initialize the map with all allergies and "geen" with a value of 0
+const initialPersonsMap = allergies.reduce((map, allergy) => {
+    map[allergy] = 0;
+    return map;
+}, {});
+
+export const persons = persistentAtom<{ [key: string]: number }>('persons', initialPersonsMap, {
+    encode: JSON.stringify,
+    decode: JSON.parse,
+});
+
