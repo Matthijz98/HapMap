e2ee21a97f2e769a8eed6646e883e2ea726bbbbe
Matthijs Zondervan <mzondervan@hotmail.nl>
2024-05-21 17:28:04 +0200

    fix page-indexing, mobile layout and update

--- src/components/ingredients/Ingredients.tsx
+++ src/components/ingredients/Ingredients.tsx
@@ -3,8 +3,8 @@ import type {RecipeIngredientType} from '../../content/config.ts';
 
 export default function Ingredients({ingredients}: {ingredients: RecipeIngredientType[]}) {
     return (
-        <div className={"bg-slate-200 rounded mb-4"}>
-            <table className={'w-full'}>
+        <div className={"bg-slate-200 rounded mb-4 overflow-x-auto"}>
+            <table className={'w-full text-nowrap whitespace-nowrap'}>
                 <thead className={'text-left'}>
                 <tr className={'border-b-2 border-slate-400'}>
                     <th className={"p-2"}>Ingredient</th>
