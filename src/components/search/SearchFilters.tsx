ae9991e0392d8bf472fcd5c1ea5e31a128a359e2
Matthijs Zondervan <mzondervan@hotmail.nl>
2024-08-09 14:37:32 +0200

    try to add more filters

--- src/components/search/SearchFilters.tsx
+++ src/components/search/SearchFilters.tsx
@@ -6,9 +6,6 @@ export default function SearchFilters({ filters }) {
     const isVisible = useStore(filtersVisibilityStore)
     const activeFilters = useStore(activeFiltersStore)
 
-    console.log('activeFilters', activeFilters)
-    console.log('filters', filters)
-
     const toggleVisibility = () => {
         filtersVisibilityStore.set(!isVisible)
     }
@@ -30,25 +27,39 @@ export default function SearchFilters({ filters }) {
 
     return (
         <div className={'text-slate-300 bg-slate-700 rounded mt-1 p-2'}>
-            <div className={isVisible ? '' : 'hidden'}>
-                <h3>Filters:</h3>
+            <div className={isVisible ? 'flex flex-col gap-2' : 'hidden'}>
+                <h3 className={`text-center font-medium`}>Filters:</h3>
                 <div className={'flex gap-2 flex-wrap'}>
                     <h4>Categorie:</h4>
 
                     {filters.category && Object.entries(filters.category).map(([key, value]) => (
                         <button
                             key={key}
-                            className={`rounded-full text-black px-2 py-1 text-xs text-nowrap ${activeFilters.filters && activeFilters.filters.category === key ? 'bg-green-500' : 'bg-white'}`}
+                            className={`rounded-full text-black px-2 py-0.5 text-xs text-nowrap ${activeFilters.filters && activeFilters.filters.category === key ? 'bg-green-500 shadow shadow-green-600' : 'bg-white'}`}
+                            onClick={() => toggleFilter(key)}
+                        >
+                            {key} - {value}
+                        </button>
+                    ))}
+
+                </div>
+                <div className={'flex gap-2 flex-wrap'}>
+                    <h4>Vrij van:</h4>
+                    {filters.allergy && Object.entries(filters.allergy).map(([key, value]) => (
+                        <button
+                            key={key}
+                            className={`rounded-full text-black px-2 py-0.5 text-xs text-nowrap ${activeFilters.filters && activeFilters.filters.category === key ? 'bg-red-500 shadow shadow-red-500' : 'bg-white'}`}
                             onClick={() => toggleFilter(key)}
                         >
                             {key} - {value}
                         </button>
                     ))}
                 </div>
+
             </div>
 
             <button className={'w-full text-center'} onClick={toggleVisibility}>
-                {isVisible ? 'Hide filters' : 'Show filters'}
+                {isVisible ? 'Verberg filters' : 'Toon filters'}
             </button>
         </div>
     )
