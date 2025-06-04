051ffec37deddab9572b45176e2cebb7221f4143
Matthijs Zondervan <mzondervan@hotmail.nl>
2025-06-04 14:49:26 +0200

    small ui fixes

--- src/components/eaters/Eaters.svelte
+++ src/components/eaters/Eaters.svelte
@@ -18,12 +18,19 @@
 <div class="bg-slate-200 rounded p-2 mb-4">
   <div class="flex flex-col gap-4">
     <div class="flex gap-4">
-      <label class="text-nowrap">Aantal eeters:</label>
+      <label class="text-nowrap">Aantal eeters totaal:</label>
       <input class="w-full rounded px-1 py-0.5" value={$eaters} on:input={handleInputChange}/>
     </div>
-    
-    {#each allergies as allergy}
-      <EaterInput {allergy} />
-    {/each}
+    <div class="bg-slate-300 rounded p-2">
+      <div class="mb-2">
+        <h6 class="text-sm">Waarvan met een .... allergie:</h6>
+      </div>
+
+      <div class="flex flex-col gap-2">
+        {#each allergies as allergy}
+          <EaterInput {allergy} />
+        {/each}
+      </div>
+    </div>
   </div>
 </div>
