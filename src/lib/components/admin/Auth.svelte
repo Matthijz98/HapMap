1ef513b0d22f61a0fd93a9e866beaef441bb3d03
Matthijs Zondervan <mzondervan@hotmail.nl>
2026-02-09 22:37:17 +0100

    Intergrade the auth for the backend

--- src/lib/components/admin/Auth.svelte
+++ src/lib/components/admin/Auth.svelte
@@ -1,38 +1,52 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import NavBar from '$lib/Components/NavBar/NavBar.svelte';
+
 	import {PUBLIC_AUTH_CLIENT_ID, PUBLIC_API_HOST} from '$env/static/public'
 
 	import {
 		OidcContext,
 		LoginButton,
 		isAuthenticated,
+		accessToken,
 	} from '@dopry/svelte-oidc';
-	import Card from '$lib/Components/UI/Card.svelte';
+	import { client } from '$lib/api/private-client/client.gen';
 
 	let { children } = $props();
+
+	accessToken.subscribe((token: string | null) => {
+		client.interceptors.request.use(async (request) => {
+			try {
+				// Use the access token from the OIDC store
+				if (token) {
+					request.headers.set('Authorization', 'Bearer ' + token);
+				}
+			} catch (error) {
+				console.error('Failed to get OIDC access token:', error);
+			}
+			return request;
+		});
+	});
 </script>
 
 {#if browser}
 	<OidcContext
 		issuer={`${PUBLIC_API_HOST}/oauth/`}
 		client_id={PUBLIC_AUTH_CLIENT_ID}
-		redirect_uri={window.location.origin}
+		redirect_uri={`${window.location.origin}/admin/recepten`}
 	>
 		{#if $isAuthenticated}
-			<NavBar />
 			<div class="container mx-auto mt-4 p-2">
 				{@render children()}
 			</div>
 		{:else}
 			<div class="min-h-svh flex items-center justify-center">
-				<Card>
-					<img class="w-52 px-12 mx-auto" src="/logo.png" alt="blauwe schuit logo">
+				<div class="bg-slate-100 rounded p-8 text-center">
+					<img class="w-52 px-12 mx-auto" src="/logo.png" alt="HapMap logo">
 					<h2 class="mt-4 mb-6">Login met je SchuitAccount</h2>
 					<div class="flex items-center justify-center">
 						<LoginButton classes="rounded px-2 py-1 bg-slate-300 hover:bg-slate-500">Login</LoginButton>
 					</div>
-				</Card>
+				</div>
 			</div>
 
 
--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
+	import Auth from '$lib/components/admin/Auth.svelte';
+
 	let { children } = $props();
 </script>
 
-<div class="container mx-auto">
-	<div class="flex justify-between">
-		<h1 class="text-white font-black text-2xl">HapMap Admin</h1>
-		<div class="flex gap-4">
-			<a>Recepten</a>
-			<a>Handleiding</a>
-			<a>Stats</a>
+<Auth>
+	<div class="container mx-auto">
+		<div class="flex justify-between">
+			<h1 class="text-white font-black text-2xl">HapMap Admin</h1>
+			<div class="flex gap-4">
+				<a>Recepten</a>
+				<a>Handleiding</a>
+				<a>Stats</a>
+			</div>
+			<div class="text-white font-black">Logout</div>
 		</div>
-		<div class="text-white font-black">Logout</div>
+		{@render children()}
 	</div>
-	{@render children()}
-</div>
+</Auth>
